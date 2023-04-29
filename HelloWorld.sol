a1e8af19e92da43ca511922c32af2e1dae9fe538
ynvygy <alexandru_musat@yahoo.com>
2023-04-29 10:09:55 +0300

    owners as array concept

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -3,27 +3,47 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract HelloWorld {
     string private text;
-    address public owner;
+    // owners concept
+    address[] public owners;
 
     constructor() {
         text = "Hello World";
-        owner = msg.sender;
+        owners.push(msg.sender);
     }
 
     function helloWorld() public view returns (string memory) {
         return text;
     }
 
-    function setText(string calldata newText) public onlyOwner {
+    function setText(string calldata newText) public onlyOwners {
         text = newText;
     }
 
-    function transferOwnership(address newOwner) public onlyOwner {
-        owner = newOwner;
+    function addOwner(address newOwner) public onlyOwners {
+        owners.push(newOwner);
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Caller is not the owner");
+    function giveUpOwnership() public onlyOwners {
+        // Don't allow the last owner to give up
+        require(owners.length > 1, "You can't revoke your ownership");
+        for (uint i = 0; i < owners.length; i++) {
+            if (msg.sender == owners[i]) {
+                owners[i] = owners[owners.length - 1];
+                owners.pop();
+                break;
+            }
+        }
+    }
+
+    modifier onlyOwners() {
+        bool isOwner;
+        for (uint i = 0; i < owners.length; i++) {
+            if (msg.sender == owners[i]) {
+                isOwner = true;
+                break;
+            }
+        }
+        require(isOwner, "Caller is not the owner");
         _;
     }
 }
